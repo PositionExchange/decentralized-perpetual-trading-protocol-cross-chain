659ee0dc30ccc672f5f29ea712b1b574cf9a00f8
LiamPosition <liam.h@position.exchange>
2023-05-08 11:18:33 +0700

    deploy referral storage

--- deploy/migrations/009-deploy-referral-storage.ts
+++ deploy/migrations/009-deploy-referral-storage.ts
@@ -1,12 +1,11 @@
 import { MigrationContext, MigrationDefinition } from "../types";
-import { GatewayUtils, WETH } from "../../typeChain";
-import { BTCBUSD, ETHBUSD, LINKBUSD } from "../config_production";
-import { ContractTransaction } from "ethers";
 
 const migrations: MigrationDefinition = {
   getTasks: (ctx: MigrationContext) => ({
     "deploy referral storage": async () => {
-      const referralStorage = await ctx.factory.db.findAddressByKey("ReferralStorage");
+      const referralStorage = await ctx.factory.db.findAddressByKey(
+        "ReferralStorage"
+      );
       await ctx.factory.createReferralStorage(referralStorage);
     },
   }),
