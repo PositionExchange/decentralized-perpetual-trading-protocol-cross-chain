f9b2138e91384942b4cec66847534b5ea47e2ea7
MusketPosition <musket.gun@position.exchange>
2023-05-11 16:10:32 +0700

    add contract vault reader

--- contracts/interfaces/IFuturXGateway.sol
+++ contracts/interfaces/IFuturXGateway.sol
@@ -2,4 +2,8 @@ pragma solidity ^0.8.0;
 
 interface IFuturXGateway {
     function executionFee() external returns (uint256);
+
+    function maxGlobalShortSizes(address token) external view returns(uint256);
+
+    function maxGlobalLongSizes(address token) external view returns(uint256);
 }
