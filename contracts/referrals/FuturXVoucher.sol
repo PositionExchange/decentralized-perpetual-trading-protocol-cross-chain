cedbc3f027e76da471f7adcb85ff4d2c8dccb9f1
MusketPosition <musket.gun@position.exchange>
2023-05-22 16:57:19 +0700

    using ERC721Enumerable

--- contracts/referrals/FuturXVoucher.sol
+++ contracts/referrals/FuturXVoucher.sol
@@ -6,9 +6,10 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 
-contract FuturXVoucher is ERC721, Ownable {
+contract FuturXVoucher is  ERC721Enumerable,  Ownable{
 
     struct Voucher {
         address owner;
