e431660f9a77d2da9b1fb3baf6735491615bd300
MusketPosition <musket.gun@position.exchange>
2023-06-22 15:12:05 +0700

    config

--- deploy/config_production.ts
+++ deploy/config_production.ts
@@ -182,9 +182,9 @@ export const UNIBUSD : PositionManagerConfigData = {
 }
 
 export const ARB_RELAYERS = [
-    "0x"
+    "0x5d0ea84a7f79feEBD9344c57981F717AB6c714cF"
 ]
 
 export const ARB_REFUNDERS = [
-    "0x"
+    "0x0A1B5427fBA7533DC1257A1CaA493a2Ad51C22cF"
 ]
